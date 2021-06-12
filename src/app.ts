3b6a1eb96a9e22e99f1bc71c6b331cd90ac240b0
tuckermoses <john.tucker.moses@gmail.com>
2021-06-12 12:05:26 -0700

    setup part 2

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.get('/apicall', (_, res) => {
 app.use(express.static(static_root))
 
 app.get('*', (_, res) => {
-  res.sendFile('index.html', { static_root });
+  res.sendFile('index.html', { root: static_root });
 });
 
 app.listen(port, () => {
--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -1,7 +1,59 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // apply postcss plugins
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  'postcss-import',
+                  'autoprefixer',
+                  'postcss-flexbugs-fixes',
+                  [
+                    'postcss-preset-env', 
+                    {
+                      autoprefixer: {
+                        flexbox: 'no-2009',
+                      },
+                      stage: 3,
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
