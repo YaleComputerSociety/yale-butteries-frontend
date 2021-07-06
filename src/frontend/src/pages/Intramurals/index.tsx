63adc793fef2f718db2ae9a1e15a245c99259f21
Kelvin Yip <kelvinyip7@Kelvins-MBP.home>
2021-07-05 20:24:04 -0400

    Remove front

--- src/frontend/src/pages/Intramurals/index.tsx
+++ src/frontend/src/pages/Intramurals/index.tsx
@@ -1,48 +1,9 @@
 import React, { FC } from 'react'
 
-import Default from 'layouts/Default/'
-
-const SAMPLEPLAYERS = [
-  {
-    name: 'Tucker Moses',
-    minute: 30,
-    rebound: 5,
-    assist: 6,
-    pts: 3,
-  },
-  {
-    name: 'Tucker Moses',
-    minute: 30,
-    rebound: 5,
-    assist: 6,
-    pts: 3,
-  },
-  {
-    name: 'Tucker Moses',
-    minute: 30,
-    rebound: 5,
-    assist: 6,
-    pts: 3,
-  },
-]
-
-const statSheet = [
-  {
-    homeScore: 21,
-    homePlayers: SAMPLEPLAYERS,
-    awayScore: 17,
-    awayPlayers: SAMPLEPLAYERS,
-  },
-  {
-    homeScore: 39,
-    homePlayers: SAMPLEPLAYERS,
-    awayScore: 58,
-    awayPlayers: SAMPLEPLAYERS,
-  },
-]
+import Default from '../../layouts/Default'
 
 const Inner: FC = () => {
-  return <></>
+  return <div>{'Intramurals logic'}</div>
 }
 
 const Intramurals: FC = () => {
