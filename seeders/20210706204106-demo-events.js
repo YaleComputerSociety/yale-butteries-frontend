b8f5150e16548ff711f27bc971420aeee6d2c1ed
tuckermoses <john.tucker.moses@gmail.com>
2021-07-11 00:21:43 -0700

    updated seeders

--- seeders/20210706204106-demo-events.js
+++ seeders/20210706204106-demo-events.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface) => {
     /**
      * Add seed commands here.
      *
@@ -16,10 +16,9 @@ module.exports = {
       {
         name: 'Event 1',
         description: 'Event 1',
-        start_time: '2021-07-04T16:00:00.000Z',
-        end_time: '2021-07-04T16:01:00.000Z',
         created_at: new Date(),
         updated_at: new Date(),
+        user_id: 1,
         event_type_id: 1,
         recurrence_type_id: 2,
         room_id: 1,
@@ -28,22 +27,20 @@ module.exports = {
       {
         name: 'Event 2',
         description: 'Event 2',
-        start_time: '2021-07-05T16:00:00.000Z',
-        end_time: '2021-07-05T16:01:00.000Z',
         created_at: new Date(),
         updated_at: new Date(),
+        user_id: 2,
         event_type_id: 1,
-        recurrence_type_id: 2,
+        recurrence_type_id: 1,
         room_id: 2,
         approval_status_id: 3,
       },
       {
         name: 'Event 3',
         description: 'Event 3',
-        start_time: '2021-07-06T16:00:00.000Z',
-        end_time: '2021-07-06T16:01:00.000Z',
         created_at: new Date(),
         updated_at: new Date(),
+        user_id: 3,
         event_type_id: 1,
         recurrence_type_id: 1,
         room_id: 3,
@@ -52,10 +49,9 @@ module.exports = {
       {
         name: 'Event 4',
         description: 'Event 4',
-        start_time: '2021-07-07T16:00:00.000Z',
-        end_time: '2021-07-07T16:01:00.000Z',
         created_at: new Date(),
         updated_at: new Date(),
+        user_id: 4,
         event_type_id: 1,
         recurrence_type_id: 2,
         room_id: 1,
@@ -64,10 +60,9 @@ module.exports = {
       {
         name: 'Event 5',
         description: 'Event 5',
-        start_time: '2021-07-08T16:00:00.000Z',
-        end_time: '2021-07-08T16:01:00.000Z',
         created_at: new Date(),
         updated_at: new Date(),
+        user_id: 5,
         event_type_id: 1,
         recurrence_type_id: 3,
         room_id: 2,
@@ -76,7 +71,7 @@ module.exports = {
     ])
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     /**
      * Add commands to revert seed here.
      *
