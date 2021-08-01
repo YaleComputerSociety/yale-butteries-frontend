da98dc191b70c1a7e1fcd57a793c3e93115e5b4c
tuckermoses <john.tucker.moses@gmail.com>
2021-07-31 18:27:59 -0700

    made events changes

--- seeders/20210706204106-demo-events.js
+++ seeders/20210706204106-demo-events.js
@@ -16,6 +16,9 @@ module.exports = {
       {
         name: 'Event 1',
         description: 'Event 1',
+        first_start_timestamp: '2021-07-04T16:00:00.000Z',
+        first_end_timestamp: '2021-07-04T16:01:00.000Z',
+        end_date: '2021-07-18T16:00:00.000Z',
         created_at: new Date(),
         updated_at: new Date(),
         user_id: 1,
@@ -27,6 +30,9 @@ module.exports = {
       {
         name: 'Event 2',
         description: 'Event 2',
+        first_start_timestamp: '2021-07-05T14:00:00.000Z',
+        first_end_timestamp: '2021-07-05T14:01:00.000Z',
+        end_date: '2021-07-19T14:00:00.000Z',
         created_at: new Date(),
         updated_at: new Date(),
         user_id: 2,
@@ -38,6 +44,9 @@ module.exports = {
       {
         name: 'Event 3',
         description: 'Event 3',
+        first_start_timestamp: '2021-07-06T18:00:00.000Z',
+        first_end_timestamp: '2021-07-06T18:01:00.000Z',
+        end_date: '2021-07-08T18:00:00.000Z',
         created_at: new Date(),
         updated_at: new Date(),
         user_id: 3,
@@ -49,6 +58,9 @@ module.exports = {
       {
         name: 'Event 4',
         description: 'Event 4',
+        first_start_timestamp: '2021-08-07T12:00:00.000Z',
+        first_end_timestamp: '2021-08-07T12:01:00.000Z',
+        end_date: '2021-08-21T12:00:00.000Z',
         created_at: new Date(),
         updated_at: new Date(),
         user_id: 4,
@@ -60,6 +72,9 @@ module.exports = {
       {
         name: 'Event 5',
         description: 'Event 5',
+        first_start_timestamp: '2021-07-25T15:00:00.000Z',
+        first_end_timestamp: '2021-07-25T15:01:00.000Z',
+        end_date: '2021-09-25T15:00:00.000Z',
         created_at: new Date(),
         updated_at: new Date(),
         user_id: 5,
