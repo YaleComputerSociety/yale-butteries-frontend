784b437de931f300e967b7712806d6702ef31687
tuckermoses <john.tucker.moses@gmail.com>
2021-08-17 11:58:57 -0700

    first store slices open

--- src/frontend/src/pages/Testing/index.tsx
+++ src/frontend/src/pages/Testing/index.tsx
@@ -1,19 +1,60 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect } from 'react'
+
+import { asyncFetchEvents } from 'store/slices/Events'
+import { asyncFetchEventOccurrences } from 'store/slices/EventOccurrences'
+import { useAppSelector, useAppDispatch } from 'store/TypedHooks'
+import { getEventOccurrencesWithEvents } from 'store/selectors'
 
 import Default from 'layouts/Default'
+
 import { getJSON } from 'utils/fetch'
 
 const Inner: FC = () => {
   const pathPrefix = '/api/users'
+
+  const { events, isLoading: isLoadingEvents } = useAppSelector((state) => state.events)
+  const { eventOccurrences, isLoading: isLoadingEventOccurrences } = useAppSelector((state) => state.eventOccurrences)
+  const eventOccurrencesWithEvents = useAppSelector(getEventOccurrencesWithEvents)
+
+  const dispatch = useAppDispatch()
 
   async function makeAPICall() {
     const results = await getJSON(pathPrefix)
     console.log(results)
   }
 
+  useEffect(() => {
+    if (events == null) {
+      dispatch(asyncFetchEvents())
+      console.log('run it')
+    }
+    if (eventOccurrences == null) {
+      dispatch(asyncFetchEventOccurrences())
+      console.log('run it')
+    }
+  }, [dispatch, events, eventOccurrences])
+
   return (
     <>
-      <button onClick={makeAPICall}>{'Api Call'}</button>
+      {/* {eventOccurrences == null && <button onClick={loadData}>{'Api Call'}</button>} */}
+      {(isLoadingEventOccurrences == true || isLoadingEvents == true) && <div>{'loading...'}</div>}
+      {eventOccurrencesWithEvents != null &&
+        eventOccurrencesWithEvents.map((eventOccurrenceWithEvent) => {
+          return (
+            <ul key={eventOccurrenceWithEvent.eventOccurrence.id}>
+              <li>{'Event Occurrence:'}</li>
+              <li>{eventOccurrenceWithEvent.eventOccurrence.id}</li>
+              <li>{eventOccurrenceWithEvent.eventOccurrence.start_time}</li>
+              <li>{eventOccurrenceWithEvent.eventOccurrence.end_time}</li>
+              <li>{'Event:'}</li>
+              <li>{eventOccurrenceWithEvent.event.id}</li>
+              <li>{eventOccurrenceWithEvent.event.name}</li>
+              <li>{eventOccurrenceWithEvent.event.description}</li>
+              <li>{eventOccurrenceWithEvent.event.updatedAt}</li>
+              <li>{eventOccurrenceWithEvent.event.createdAt}</li>
+            </ul>
+          )
+        })}
     </>
   )
 }
