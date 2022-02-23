988a6e047728f5cddb52863782f31f7b39a12579
Addison Goolsbee <addisongoolsbee@gmail.com>
2022-02-22 23:58:22 -0500

    open time works

--- App.tsx
+++ App.tsx
@@ -52,14 +52,3 @@ export default function App() {
 
       </Stack.Navigator>
     </NavigationContainer> */
-
-    // <ScrollView style={homeStyles.app} showsVerticalScrollIndicator={false} >
-    //   <View style={homeStyles.menuView}>
-    //     <MenuItem name='Chicken Sandwich' price={1.75} description="Not only is there a chicken, but as part of a limited time special offer, we're adding two additional flaps of bread."/>
-    //     <MenuItem name='Milkshake' description="I'm cold." price={2.5}/>
-    //     <MenuItem name='Floor Scraps' description="An economical choice" price={0.6}/>
-    //     <MenuItem name='Face Slap' description="Not necessarily food, but refreshing nonetheless" price={0.06}/>
-    //     <MenuItem name='Burger' description="No, this was NOT stolen from the dining hall" price={2.044}/>
-    //     <MenuItem name='Chicken Stir Fry' description="Fried rice with eggs and chicken" price={2}/>
-    //   </View>
-    // </ScrollView>
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { View, ImageBackground, Text, Image} from 'react-native';
+import { View, ImageBackground, Text, Image, Pressable} from 'react-native';
 import {homeStyles, cardStyles} from '../styles/HomeStyles';
 
 export const Card = (props:any) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
+  const [openTimeHours, setOpenTimeHours] = useState(0);
+  const [closeTimeHours, setCloseTimeHours] = useState(0);
+  const [openTimeMinutes, setOpenTimeMinutes] = useState(0);
+  const [closeTimeMinutes, setCloseTimeMinutes] = useState(0);
 
   // determines whether the buttery is currently open
   function currentlyOpen(){
-    const a = props.openTimeHours;
-    const b = props.closeTimeHours;
-    const am = props.openTimeMinutes;
-    const bm = props.closeTimeMinutes;
+    const a = openTimeHours;
+    const b = closeTimeHours;
+    const am = openTimeMinutes;
+    const bm = closeTimeMinutes;
     const h = new Date().getHours();
     const m = new Date().getMinutes();
     if (a<b){ // standard case
@@ -23,38 +27,44 @@ export const Card = (props:any) => {
     }
   }
 
-  // check every 0.5 seconds whether the buttery is open :( 
+  // immediately check if the buttery is open
+  useEffect(() => {
+    setIsOpen(currentlyOpen());
+  }, [isOpen]);
+
+  //check every minute whether the buttery is open
   useEffect(() => {
     const interval = setInterval(() => {
-      //console.log(currentlyOpen());
       setIsOpen(currentlyOpen());
-    }, 1000);
+    }, 60000);
     return () => clearInterval(interval);
-  }, []);
+  }, [isOpen]);
+
+ 
+
+  // translate opentime/close time into openTimeHours etc
+  useEffect(() => {
+    setOpenTimeHours(parseInt(props.openTime.substring(0, props.openTime.indexOf(':'))) + (props.openTime.toString().includes("pm") ? 12 : 0));
+    setOpenTimeMinutes(parseInt(props.openTime.substring(props.openTime.indexOf(':')+1)));
+
+    setCloseTimeHours(parseInt(props.closeTime.substring(0, props.closeTime.indexOf(':'))) + (props.closeTime.toString().includes("pm") ? 12 : 0));
+    setCloseTimeMinutes(parseInt(props.closeTime.substring(props.closeTime.indexOf(':')+1)));
+
+  }, [props.openTime, props.closeTime]);
 
   // takes openTime and closeTime and puts them into clean text form. Assumes (h)h:(m)m form with optional pm/am
-  // also stores the props openTimeHours, openTimeMinutes, closeTimeHours, and closeTimeMinutes
-  // DOESN'T WORK ON FIRST RENDER - HOW DO I FIX THIS
   function cleanTime(){
     // get open time
-    const openTimeHours = parseInt(props.openTime.substring(0, props.openTime.indexOf(':'))) + (props.openTime.toString().includes("pm") ? 12 : 0);
-    const openTimeMinutes = parseInt(props.openTime.substring(props.openTime.indexOf(':')+1));
     const cleanOpen =  (openTimeHours%12) + ':' + (openTimeMinutes<10 ? '0': '') + openTimeMinutes + (openTimeHours>12 ? 'pm' : 'am');
-    
-    // get close time
-    const closeTimeHours = parseInt(props.closeTime.substring(0, props.closeTime.indexOf(':'))) + (props.closeTime.toString().includes("pm") ? 12 : 0);
-    const closeTimeMinutes = parseInt(props.closeTime.substring(props.closeTime.indexOf(':')+1));
     const cleanClose =  (closeTimeHours%12) + ':' + (closeTimeMinutes<10 ? '0': '') + closeTimeMinutes + (closeTimeHours>12 ? 'pm' : 'am');
     
     return cleanOpen + ' - ' + cleanClose;
   }
 
-  var cleanedTime = cleanTime();
-  //console.log(props.openTimeHours);
-
   return (
 
-  <ImageBackground source={props.backgroundImage} resizeMode="cover" style={[cardStyles.card, isOpen ? cardStyles.cardOpen : cardStyles.cardClosed]} imageStyle={{borderRadius: 6}}>
+    <Pressable onPress={props.onPress} disabled={!isOpen}>
+      <ImageBackground source={props.backgroundImage} resizeMode="cover" style={isOpen ? cardStyles.card : [cardStyles.card, {opacity: 0.5}]} imageStyle={{borderRadius: 6}}>
       <View style={cardStyles.cardContent}>
         <View>
           <Text style={cardStyles.cardText1}>{props.college}</Text>
@@ -62,7 +72,8 @@ export const Card = (props:any) => {
         </View>
         <Image style={cardStyles.butteryIcon} source={props.image}/>
       </View>
-  </ImageBackground>
+    </ImageBackground>
+  </Pressable>
   );
 }
 
