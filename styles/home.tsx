287992579a7f5090c063db6b598256c3969e0fd7
apalm3 <apalmer21379@gmail.com>
2022-02-22 21:50:20 -0500

    merge w/ addision

--- styles/home.tsx
+++ styles/home.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const homeStyles = StyleSheet.create({
   app: {
     flex:1,
-    backgroundColor: '#eaeaea', //'#343a40',
+    backgroundColor: '#eaeaea',//'#343a40',
   },
 
   container:{
@@ -34,8 +34,8 @@ export const homeStyles = StyleSheet.create({
   },
 
   menuView: { 
-    flex: 1, 
-    alignItems: 'center', 
+    display: 'flex',
+    alignItems: 'stretch', 
     justifyContent: 'center',
   },
 })
@@ -83,10 +83,88 @@ export const cardStyles = StyleSheet.create({
   
   cardContent: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
     marginHorizontal: 10,
     justifyContent: 'space-between',
     paddingLeft:10,
     opacity: 1,
   },
+})
+
+export const itemStyles = StyleSheet.create({
+  card: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    height: 120,
+    elevation: 3, 
+    backgroundColor: '#fff',
+    borderRadius: 6,
+    shadowRadius: 2,
+    margin: 12,
+    marginBottom: 0,
+  },
+
+  leftSide: {
+    flex: 6,
+  },
+
+
+  itemName: {
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    color: '#222',
+    fontSize: 20,
+  },
+
+  itemDescription: {
+    fontFamily: 'Roboto',
+    color: '#777',
+    fontSize: 12,
+    flex: 2,
+    lineHeight: 12,
+  },
+
+  itemPrice: {
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    color: '#222',
+    fontSize: 20,
+  },
+
+  button: {
+    elevation: 6,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 0},
+    shadowOpacity: 0.4,
+    flex: 1.58,
+    borderRadius: 100,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+
+    fontSize: 30,
+    height: 40,
+  },
+
+  countText: {
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    color: '#000',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+
+  spacer: {
+    flex: 1.1,
+    justifyContent: 'center',
+  },
+
 })
