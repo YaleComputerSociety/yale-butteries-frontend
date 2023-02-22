18520e3663ae8d62741e16b59fa807eef2fbf37e
dejadunlap <70656325+dejadunlap@users.noreply.github.com>
2023-02-22 16:15:17 -0500

    staff sign in complete (mostly)

--- screens/StaffLoginScreen.tsx
+++ screens/StaffLoginScreen.tsx
@@ -1,23 +1,21 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, {useEffect, FC, useState } from 'react'
 import {View, Text, TextInput, Pressable} from 'react-native'
 import { staffLogin } from '../styles/StaffLoginStyles'
 import { LinearGradient } from 'expo-linear-gradient'
-import {STAFF_USER, STAFF_PASS} from '@env'
+//import {STAFF_USER, STAFF_PASS} from '@env'
 
 
 const StaffLoginScreen: FC<{ navigation: any }> = ({ navigation }) => {
   const [username, setUser] = useState('')
   const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('');
 
   function validateForm() {
     return username.length >= 2 && password.length >= 2
   }
 
-  function checkInfo(name) {
-    console.log("Something")
-    console.log("User: " + STAFF_USER)
-    console.log("Password: " + STAFF_PASS)
-    console.log("Something: " + name)
+  function checkInfo() {
+    return username == "username" && password == "password"
   }
 
   return (
@@ -28,12 +26,19 @@ const StaffLoginScreen: FC<{ navigation: any }> = ({ navigation }) => {
         <TextInput placeholder="username" style={staffLogin.input} onChangeText={setUser}></TextInput>
         <TextInput placeholder="password" style={staffLogin.input} onChangeText={setPassword} secureTextEntry={true}></TextInput>
         <Pressable 
-              onPress={() => {checkInfo("deja")}}
+              onPress={() => {
+                if (checkInfo()) {
+                  navigation.navigate('OrderStatusScreen')
+                } else {
+                  {setErrorMessage("Password and/or Username is Incorrect. Please Try Again")}
+                }
+              }}
               style={({ pressed }) => [{ opacity: pressed ? 0.8 : 1 }]}>
               <View style={staffLogin.button}>
                 <Text style={{ fontSize: 15, color: '#fff', fontFamily: 'HindSiliguri-Bolder' }}> Sign in</Text>
               </View>
         </Pressable>
+        {errorMessage && <Text> {errorMessage} </Text>}
       </View>
       </View>
       </LinearGradient>
