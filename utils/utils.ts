513f28aa83e9a3426ec61611fd51a20bc822be70
Lawrence Zhao <lzhao@Lawrences-Air.wireless.yale.internal>
2023-03-28 15:37:49 -0400

    weird .env fixes

--- utils/utils.ts
+++ utils/utils.ts
@@ -2,3 +2,4 @@ import { IOS_DEV_URL, ANDROID_DEV_URL } from '@env'
 import { Platform } from 'react-native'
 
 export const baseUrl = Platform.OS == 'ios' ? IOS_DEV_URL : ANDROID_DEV_URL
+// there needs to be a comment here for some peoples' baseUrls to work. Don't ask
