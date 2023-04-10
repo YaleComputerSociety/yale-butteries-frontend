20b3bd52e5495fff8b04b57aa15cebd0200f8e36
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-04-10 11:51:23 -0400

    utils merge

--- utils/utils.ts
+++ utils/utils.ts
@@ -2,5 +2,11 @@ import Constants from 'expo-constants'
 
 const { manifest } = Constants
 
-export const baseUrl = `http://${manifest.debuggerHost.split(':').shift()}:3000/`
-// there needs to be a comment here for some peoples' baseUrls to work. Don't ask why
+// this defines if we're in development (for now)
+const dev = true
+
+const production = 'https://radiant-beach-77007.herokuapp.com/'
+const development = `http://${manifest.debuggerHost.split(':').shift()}:3000/`
+
+export const baseUrl = dev ? development : production
+console.log(baseUrl)
