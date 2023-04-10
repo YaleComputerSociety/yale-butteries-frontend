a105cdfb7beaa5bc16de1b952b8d0aed3053aea2
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-04-10 11:56:29 -0400

    dev detection

--- utils/utils.ts
+++ utils/utils.ts
@@ -2,11 +2,8 @@ import Constants from 'expo-constants'
 
 const { manifest } = Constants
 
-// this defines if we're in development (for now)
-const dev = true
-
 const production = 'https://radiant-beach-77007.herokuapp.com/'
 const development = `http://${manifest.debuggerHost.split(':').shift()}:3000/`
 
-export const baseUrl = dev ? development : production
+export const baseUrl = __DEV__ ? development : production
 console.log(baseUrl)
