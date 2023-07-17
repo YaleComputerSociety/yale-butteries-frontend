49af3b5fa8f1611be3d08108a631a673d9c360e2
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-07-16 22:52:43 -0400

    Remove env file from code

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,10 +1,3 @@
-// import Constants from 'expo-constants'
-
-// eslint-disable-next-line import/no-unresolved
-// import { STRIPE_PK_DEV, STRIPE_PK_PROD } from '@env'
-
-// const { manifest } = Constants
-
 const production = 'https://yale-butteries.herokuapp.com/'
 // The manifest.debuggerHost part crashes the app if you build the app. PLEASE do not delete this comment there aren't any error logs when the build crashes and it took me more than 15 hours to find this
 // const development = `http://${manifest.debuggerHost.split(':').shift()}:3000/`
