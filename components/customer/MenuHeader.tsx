8be5b050b398f0902cee50232cea97508cb4de4c
Aidan Palmer <apalmer21379@gmail.com>
2023-07-22 10:50:34 -0400

    sectioned styling header

---
+++ components/customer/MenuHeader.tsx
@@ -0,0 +1,87 @@
+import React, { FC } from 'react'
+import { View, StyleSheet, Pressable, Text, Image } from 'react-native'
+import Ionicon from 'react-native-vector-icons/Ionicons'
+
+
+interface Props {
+    toFood: () => void
+    toDrink: () => void
+    toDessert: () => void
+  }
+
+export const MenuHeader: FC<Props> = ({ toDessert, toDrink, toFood }: Props) => {
+  return (
+    <View style={styles.wrapper}>
+        <Pressable           
+            style={({ pressed }) => [
+                { opacity: pressed ? 0.7 : 1 },
+                styles.button,
+            ]} 
+            onPress={toFood}>
+            <View style={styles.iconWrapper}>
+                <Image source={require('../../assets/images/icons8-french-fries-48.png')} style={styles.image}/>
+            </View>
+            <Text style={styles.text}>Food</Text>
+        </Pressable>
+        <Pressable 
+            style={({ pressed }) => [
+                { opacity: pressed ? 0.7 : 1 },
+                styles.button,
+            ]}  
+            onPress={toDrink}>
+            <View style={styles.iconWrapper}>
+                <Image source={require('../../assets/images/icons8-cola-48.png')} style={styles.image}/>
+            </View>
+            <Text style={styles.text}>Drink</Text>
+        </Pressable>
+        <Pressable 
+            style={({ pressed }) => [
+                { opacity: pressed ? 0.7 : 1 },
+                styles.button,
+            ]}  
+            onPress={toDessert}>
+            <View style={styles.iconWrapper}>
+                <Image source={require('../../assets/images/icons8-cupcake-48.png')} style={styles.image}/>
+            </View>
+            <Text style={styles.text}>Dessert</Text>
+        </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flexDirection: 'row',
+    },
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#eee',
+        flex: 2,
+        borderRadius: 10,
+        padding: 10,
+        marginTop: 10,
+        marginHorizontal: 10,
+        height: 115,
+        shadowColor: '#000',
+        shadowRadius: 3,
+        shadowOpacity: 0.1,
+    },
+    text: {
+        fontSize: 22,
+        color: 'black',
+        fontFamily: 'Staat',
+        marginTop: 10,
+    },
+    iconWrapper: {
+        backgroundColor: '#fff',
+        padding: 10,
+        borderRadius: 100,
+        justifyContent: 'center',
+        alignContent: 'center',
+    },
+    image: {
+        height:30,
+        width: 30,
+    }
+})
