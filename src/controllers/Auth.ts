2fe0735f60db0bc5479fbcf9967067a35d26f8be
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-11-20 23:17:18 -0600

    Fix auth a bit

--- src/controllers/Auth.ts
+++ src/controllers/Auth.ts
@@ -1,24 +1,24 @@
-import express from 'express'
+import type express from 'express'
 import passport from 'passport'
 import { Strategy } from 'passport-cas'
 
-type User = {
+interface User {
   netId: string
 }
 
-const port = process.env.PORT || 3000
-export const environment = process.env.NODE_ENV || 'development'
-export const url = environment === 'production' ? `https://yale-butteries.herokuapp.com` : `http://localhost:${port}`
+const port = process.env.PORT ?? 3000
+export const environment = process.env.NODE_ENV ?? 'development'
+export const url = environment === 'production' ? 'https://yale-butteries.herokuapp.com' : `http://localhost:${port}`
 
 passport.use(
   new Strategy(
     {
       ssoBaseURL: 'https://secure.its.yale.edu/cas',
-      serverBaseURL: url,
+      serverBaseURL: url
     },
     function (login, done) {
       return done(null, {
-        netId: login,
+        netId: login
       })
     }
   )
@@ -30,7 +30,7 @@ passport.serializeUser<User>(function (user: any, done) {
 
 passport.deserializeUser(function (netId, done) {
   done(null, {
-    netId,
+    netId
   })
 })
 
@@ -40,17 +40,17 @@ export default (app: express.Express): void | express.RequestHandler => {
 
   app.get('/cas', function (req, res, next) {
     passport.authenticate('cas', function (err, user) {
-      if (err) {
-        return next(err)
+      if (err != null) {
+        next(err); return
       }
 
-      if (!user) {
-        return res.redirect('/')
+      if (user == null) {
+        res.redirect('/'); return
       }
 
       req.logIn(user, function (err) {
-        if (err) {
-          return next(err)
+        if (err != null) {
+          next(err); return
         }
         res.send(JSON.stringify(user))
       })
