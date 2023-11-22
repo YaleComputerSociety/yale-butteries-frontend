83a6b63f2accbd6186cafc26d694d356e7602efb
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-11-22 11:20:43 -0600

    Add correct error logging for body validation

--- src/middlewares/validateBodyHandler.ts
+++ src/middlewares/validateBodyHandler.ts
@@ -1,17 +1,22 @@
 import type { Request, Response, NextFunction } from 'express'
 import { plainToInstance } from 'class-transformer'
 import { validate } from 'class-validator'
+import HTTPError from '@src/utils/httpError'
 
 export function validateBody<T extends object> (bodyType: new () => T) {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const input = plainToInstance(bodyType, req.body)
-    const errors = await validate(input)
+    try {
+      const input = plainToInstance(bodyType, req.body)
+      const errors = await validate(input)
 
-    if (errors.length > 0) {
-      res.status(400).json({ errors })
-      return
+      if (errors.length > 0) {
+        res.status(400).json({ errors })
+        throw new HTTPError(JSON.stringify(errors), 400)
+      }
+
+      next()
+    } catch (e) {
+      next(e)
     }
-
-    next()
   }
 }
