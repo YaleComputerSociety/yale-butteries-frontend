ff046bffc92b2fb5629212d17b91c05f041b1c8a
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-11-23 17:33:18 -0600

    Fix auth to work, and refactor some of auth. Transition form passport-cas to passport-cas2

--- src/controllers/Auth.ts
+++ src/controllers/Auth.ts
@@ -1,8 +1,9 @@
 import type express from 'express'
+import type { Request, Response } from 'express'
 import passport from 'passport'
-import { Strategy } from 'passport-cas'
+import { Strategy } from 'passport-cas2'
 
-interface User {
+interface AuthUser {
   netId: string
 }
 
@@ -11,47 +12,52 @@ export const environment = process.env.NODE_ENV ?? 'development'
 export const url = environment === 'production' ? 'https://yale-butteries.herokuapp.com' : `http://localhost:${port}`
 
 passport.use(
-  new Strategy(
-    {
-      ssoBaseURL: 'https://secure.its.yale.edu/cas',
-      serverBaseURL: url
-    },
-    function (login, done) {
-      return done(null, {
-        netId: login
-      })
-    }
+  new Strategy({ casURL: 'https://secure.its.yale.edu/cas' }, (username, profile, done) => {
+    // TODO: create user right here instead of with separate endpoint call
+    const user = { netId: username }
+    console.log('user ', user)
+    done(null, user)
+  }
   )
 )
 
-passport.serializeUser<User>(function (user: any, done) {
+passport.serializeUser<string>((user: AuthUser, done) => {
+  console.log('Serialize User:', user, user.netId)
   done(null, user.netId)
 })
 
-passport.deserializeUser(function (netId, done) {
-  done(null, {
-    netId
-  })
+passport.deserializeUser<AuthUser>((netId, done) => {
+  console.log('Deserialize User ID:', netId)
+  done(null, { netId })
 })
 
 export default (app: express.Express): void | express.RequestHandler => {
   app.use(passport.initialize())
   app.use(passport.session())
 
-  app.get('/cas', function (req, res, next) {
+  app.get('/cas', (req: Request, res: Response, next) => {
     passport.authenticate('cas', function (err, user) {
+      console.log('req.user: ', req.user)
+      console.log('Authentication Callback - Error:', err, 'User:', user)
       if (err != null) {
-        next(err); return
+        console.error('Authentication Error:', err)
+        next(err)
+        return
       }
 
       if (user == null) {
-        res.redirect('/'); return
+        console.log('No user found, redirecting to home.')
+        res.redirect('/')
+        return
       }
 
       req.logIn(user, function (err) {
         if (err != null) {
-          next(err); return
+          console.error('Login Error:', err)
+          next(err)
+          return
         }
+        console.log('Logged In User:', req.user)
         res.send(JSON.stringify(user))
       })
     })(req, res, next)
