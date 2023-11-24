d099e6b58666101853875b50b116b2c7541532bf
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-11-24 01:28:05 -0600

    Slightly refactor auth.ts. ALl major changes will be handled by a different ticket, as it is a major overhauling

--- src/controllers/Auth.ts
+++ src/controllers/Auth.ts
@@ -1,5 +1,5 @@
 import type express from 'express'
-import type { Request, Response } from 'express'
+import type { NextFunction, Request, Response } from 'express'
 import passport from 'passport'
 import { Strategy } from 'passport-cas2'
 
@@ -7,38 +7,29 @@ interface AuthUser {
   netId: string
 }
 
-const port = process.env.PORT ?? 3000
-export const environment = process.env.NODE_ENV ?? 'development'
-export const url = environment === 'production' ? 'https://yale-butteries.herokuapp.com' : `http://localhost:${port}`
-
 passport.use(
-  new Strategy({ casURL: 'https://secure.its.yale.edu/cas' }, (username, profile, done) => {
+  new Strategy({ casURL: 'https://secure.its.yale.edu/cas' }, (username: string, profile, done) => {
     // TODO: create user right here instead of with separate endpoint call
     const user = { netId: username }
-    console.log('user ', user)
     done(null, user)
   }
   )
 )
 
 passport.serializeUser<string>((user: AuthUser, done) => {
-  console.log('Serialize User:', user, user.netId)
   done(null, user.netId)
 })
 
 passport.deserializeUser<AuthUser>((netId, done) => {
-  console.log('Deserialize User ID:', netId)
   done(null, { netId })
 })
 
-export default (app: express.Express): void | express.RequestHandler => {
+export default (app: express.Express): void => {
   app.use(passport.initialize())
   app.use(passport.session())
 
-  app.get('/cas', (req: Request, res: Response, next) => {
-    passport.authenticate('cas', function (err, user) {
-      console.log('req.user: ', req.user)
-      console.log('Authentication Callback - Error:', err, 'User:', user)
+  app.get('/cas', (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate('cas', (err: Error | null, user: Express.User | null) => {
       if (err != null) {
         console.error('Authentication Error:', err)
         next(err)
@@ -46,19 +37,18 @@ export default (app: express.Express): void | express.RequestHandler => {
       }
 
       if (user == null) {
-        console.log('No user found, redirecting to home.')
+        console.log('No user found')
         res.redirect('/')
         return
       }
 
-      req.logIn(user, function (err) {
+      req.logIn(user, (err: Error) => {
         if (err != null) {
           console.error('Login Error:', err)
           next(err)
           return
         }
-        console.log('Logged In User:', req.user)
-        res.send(JSON.stringify(user))
+        res.json(user)
       })
     })(req, res, next)
   })
