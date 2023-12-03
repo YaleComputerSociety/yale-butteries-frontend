7a73bbdc03e6c382569ba18b77cf1bfc0222d3b2
Aidan Palmer <apalmer21379@gmail.com>
2023-12-02 20:27:37 -0500

    first commit, added analytics screen

---
+++ screens/staff/AnalyticsScreen.tsx
@@ -0,0 +1,63 @@
+import React, { FC, useEffect, useState } from 'react'
+import { Text, View, ScrollView, StyleSheet, Pressable } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useAppDispatch, useAppSelector } from '../../store/ReduxStore'
+import { asyncFetchAllTransactionHistories } from '../../store/slices/TransactionHistory'
+import Ionicon from 'react-native-vector-icons/Ionicons'
+
+const AnalyticsScreen: FC = () => {
+  const dispatch = useAppDispatch()
+  const { currentUser } = useAppSelector((state) => state.currentUser)
+
+  const today = new Date()
+
+  const [date, updateDate] = useState(today)
+
+  const moveForward = () => {
+    date.setDate(date.getDate() + 1)
+    updateDate(new Date(date))
+  }
+
+  const moveBackward = () => {
+    date.setDate(date.getDate() - 1)
+    updateDate(new Date(date))
+  }
+
+  useEffect(() => {}, [updateDate])
+
+  return (
+    <ScrollView>
+      <View style={styles.dateContainer}>
+        <Pressable onPress={moveBackward}>
+          <Ionicon name="arrow-back" size={25} color="#000" />
+        </Pressable>
+        <Text style={styles.dateHeader}>{date.toDateString()}</Text>
+        <Pressable onPress={moveForward}>
+          <Ionicon name="arrow-forward" size={25} color="#000" />
+        </Pressable>
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  dateContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: 'grey',
+    width: '70%',
+    justifyContent: 'space-evenly',
+    margin: 10,
+    padding: 10,
+    borderRadius: 8,
+  },
+  dateHeader: {
+    textAlign: 'center',
+    fontSize: 20,
+    marginHorizontal: 15,
+    fontFamily: 'HindSiliguri-Bold',
+  },
+})
+
+export default AnalyticsScreen
